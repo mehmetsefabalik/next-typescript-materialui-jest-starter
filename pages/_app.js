5b98b6103833df2306c6d7cefdf6653e9aa06d16
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-04-10 00:06:50 +0300

    fix style for material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,14 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "../theme";
 
 export default class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-        </Head>
+      <Html lang="en">
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
@@ -15,5 +15,29 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      <React.Fragment key="styles">
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </React.Fragment>,
+    ],
+  };
+};
 
 export default MyDocument;
